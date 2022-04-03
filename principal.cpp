e296bede454ba8bb7a3792b2b922e1eaad677d06
alberto <alberto.diez@bosonit.com>
2022-04-03 09:34:14 +0200

    4

--- principal.cpp
+++ principal.cpp
@@ -91,7 +91,7 @@ void ArduinoConnection ::wait()
             this->lastRead = aux;
             this->state = 0;
         }
-        delay(5);
+        delay(20);
         this->counter++;
         if (this->counter == 3000)
         {
