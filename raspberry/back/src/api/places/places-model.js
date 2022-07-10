6eb53cbccc9aeecd92d20834d2e1a7b80567c5bc
alberto <alberto.diez@bosonit.com>
2022-07-10 17:43:22 +0200

    fix

--- raspberry/back/src/api/places/places-model.js
+++ raspberry/back/src/api/places/places-model.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import { sequelize } from "../../database/database"; // importamos la cadena de conexion
 
 
-const Place = sequelize.define('places', {
+const Place = sequelize.define('place', {
     latitude: {
         type: Sequelize.INTEGER,
         primaryKey: true,
