5c1b9c321e6d6f6a7066a825699a16e691410da8
alberto <alberto.diez@bosonit.com>
2022-11-30 22:14:51 +0100

    refactor: reorganice classes

--- raspberry/principalSerie.cpp
+++ raspberry/principalSerie.cpp
@@ -17,33 +17,21 @@
 
 class ArduinoConnection
 {
-    int state;
-    char bufferOut[500], bufferIn[500];
-    int lenghtBufferIn, lenghtBufferOut;
-    int nowBufferIn, actualBufferOut;
-    int counter;
-    int bitOUT, bitIN, bitCLK;
-    bool vibration, obstacle, light, fire, writeToBBDD;
-    int binary_values, has_persons, has_sound, has_gas, has_oil, has_rain, temperature, humidity;
+private:
+    static ArduinoConnection *instance;
+
+public:
+    static ArduinoConnection *getInstance(char *name)
+    {
+        if (instance == NULL)
+            instance = new ArduinoConnection(name);
+
+        return instance;
+    }
     int open_serial_port(char *name);
     int read_from_serial(int fd, char *buf, int lon);
     int fd;
-
-public:
     ArduinoConnection(char *name);
-    void getSensorValues();
-    int sendActionToRaspberry(int action);
-    int loadData(char *buffer, int lenght);
-    int getBinaryValues() { return this->binary_values; };
-    int getHasPersons() { return this->has_persons; };
-    int getHasSound() { return this->has_sound; };
-    int getHasGas() { return this->has_gas; };
-    int getHasOil() { return this->has_oil; };
-    int getHasRain() { return this->has_rain; };
-    int getTemperature() { return this->temperature; };
-    int getHumidity() { return this->humidity; };
-    int getWriteToBDD() { return this->writeToBBDD; }
-    void resetWriteToBBDD() { this->writeToBBDD = false; };
 };
 
 int ArduinoConnection::open_serial_port(char *name)
@@ -130,14 +118,45 @@ int ArduinoConnection::read_from_serial(int fd, char *buf, int lon)
 
 ArduinoConnection::ArduinoConnection(char *name)
 {
-    this->writeToBBDD = false;
     this->fd = this->open_serial_port(name);
 }
 
-int ArduinoConnection::loadData(char *buffer, int lenght)
+void delay(float millis)
+{
+    clock_t start_time = clock();
+    while (clock() < start_time + ((CLOCKS_PER_SEC / 1000.) * millis))
+    {
+    }
+}
+
+class ArduinoCall
+{
+protected:
+    ArduinoConnection *arduinoConnection;
+    int binary_values, has_persons, has_sound, has_gas, has_oil, has_rain, temperature, humidity;
+
+public:
+    ArduinoCall(ArduinoConnection *arduinoConnection)
+    {
+        this->arduinoConnection = arduinoConnection;
+    }
+    int deserialize(char *buffer, int lenght);
+    int getBinaryValues() { return this->binary_values; };
+    int getHasPersons() { return this->has_persons; };
+    int getHasSound() { return this->has_sound; };
+    int getHasGas() { return this->has_gas; };
+    int getHasOil() { return this->has_oil; };
+    int getHasRain() { return this->has_rain; };
+    int getTemperature() { return this->temperature; };
+    int getHumidity() { return this->humidity; };
+
+    bool callToArduino(int action) { return true; };
+};
+
+int ArduinoCall::deserialize(char *buffer, int lenght)
 {
     // need change
-    int aux[12];
+    int aux[120];
     sscanf(buffer, "%d %d %d %d %d %d %d %d %d %d %d %d", &aux[0], &aux[1], &aux[2], &aux[3], &aux[4],
            &aux[5], &aux[6], &aux[7], &aux[8], &aux[9], &aux[10], &aux[11]);
     if (aux[11] != (aux[0] + aux[1] + aux[2] + aux[3] + aux[4] + aux[5] + aux[6] + aux[7] + aux[8] + aux[9] + aux[10]))
@@ -156,124 +175,131 @@ int ArduinoConnection::loadData(char *buffer, int lenght)
     return 1;
 }
 
-void delay(float millis)
+class CallSensors : public ArduinoCall
 {
-    clock_t start_time = clock();
-    while (clock() < start_time + ((CLOCKS_PER_SEC / 1000.) * millis))
+public:
+    CallSensors(ArduinoConnection *Connection) : ArduinoCall(Connection)
     {
     }
-}
+    bool callToArduino()
+    {
+        int lenghtBufferIn = 500;
+        char bufferIn[500];
+        sleep(1);
+        char toSend[] = "0 1 1 0 0 2     \n\0";
+        printf("Se manda: %s", toSend);
+        if ((write(this->arduinoConnection->fd, toSend, sizeof(toSend))) == -1)
+        {
+            fprintf(stderr, "write() failed: %s\n", strerror(errno));
+            return false;
+        }
+        sleep(1);
+        this->arduinoConnection->read_from_serial(this->arduinoConnection->fd, bufferIn, 500);
+        if (this->deserialize(bufferIn, lenghtBufferIn))
+        {
+            printf("Paquete recibido: %s\n", bufferIn);
+            printf("Paquete con checksum correcto\n\n");
+            printf("Byte binarios %u\n", this->getBinaryValues());
+            if (this->getBinaryValues() & 1)
+                printf("Hay vibración\n");
+            else
+                printf("No hay vibración\n");
+            if (this->getBinaryValues() & 2)
+                printf("No hay obtaculo\n");
+            else
+                printf("Hay obstaculo\n");
+            if ((this->getBinaryValues() & 4))
+                printf("No luz\n");
+            else
+                printf("Hay luz\n");
+            if (this->getBinaryValues() & 8)
+                printf("No hay fuego\n");
+            else
+                printf("Hay fuego\n");
+            if (this->getBinaryValues() > 350)
+                printf("Personas detectadas\n");
+            else
+                printf("Personas NO detectadas\n");
+            if (this->getBinaryValues() > 600)
+                printf("Sonido detectado \n");
+            else
+                printf("Sonido NO detectado \n");
+            if (this->getBinaryValues() > 350)
+                printf("Gas detectado\n");
+            else
+                printf("Gas NO detectado\n");
+            if (this->getBinaryValues() < 500)
+                printf("Aceite detectado\n");
+            else
+                printf("Aceite NO detectado\n");
+            if (this->getBinaryValues() < 500)
+                printf("Lluvia detectado\n");
+            else
+                printf("Lluvia NO detectado\n");
+            printf("Humedad: %u\n", this->getHumidity());
+            printf("Temperatura: %u\n", this->getTemperature());
+        }
+        else
+        {
+            printf("Paquete con checksum incorrecto\n\n");
+            return false;
+        }
+        return true;
+    }
+};
 
-int ArduinoConnection ::sendActionToRaspberry(int action)
+class CallOutputs : public ArduinoCall
 {
-    sleep(1);
-    char activate = 1;
-    char output1 = 0;
-    char output2 = 0;
-    char output3 = 0;
-    char output4 = 0;
-    char toSend[] = "1 1 1 0 0 0     \n\0";
-    if (action != -1)
+public:
+    CallOutputs(ArduinoConnection *Connection) : ArduinoCall(Connection)
     {
-        if (action & 0x01)
-            output1 = 1;
-        if (action & 0x02)
-            output2 = 1;
-        if (action & 0x04)
-            output3 = 1;
-        if (action & 0x08)
-            output4 = 1;
     }
-    toSend[0] = activate + '0';
-    toSend[2] = output1 + '0';
-    toSend[4] = output2 + '0';
-    toSend[6] = output3 + '0';
-    toSend[8] = output4 + '0';
-    char checksum = activate + output1 + output2 + output3 + output4;
-    toSend[10] = checksum + '0';
-    printf("Se manda: %s", toSend);
-    if ((write(fd, toSend, sizeof(toSend))) == -1)
+    bool callToArduino(int action)
     {
-        fprintf(stderr, "write() failed: %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-    sleep(1);
-    this->read_from_serial(this->fd, this->bufferIn, 500);
-    if (loadData(this->bufferIn, lenghtBufferIn))
-    {
-        printf("Action updated:  %s\n", this->bufferIn);
-        return 1;
-    }
-    printf("Fail to send action: %s\n", this->bufferIn);
-    return 0;
-}
-
-void ArduinoConnection ::getSensorValues()
-{
-    sleep(1);
-    char toSend[] = "0 1 1 0 0 2     \n\0";
-    printf("Se manda: %s", toSend);
-    if ((write(fd, toSend, sizeof(toSend))) == -1)
-    {
-        fprintf(stderr, "write() failed: %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-    sleep(1);
-    // delay(300);
-    //    int msToDelay = 1;
-
-    this->read_from_serial(this->fd, this->bufferIn, 500);
-    if (loadData(bufferIn, lenghtBufferIn))
-    {
-        this->writeToBBDD = true;
-        printf("Paquete recibido: %s\n", bufferIn);
-        printf("Paquete con checksum correcto\n\n");
-        printf("Byte binarios %u\n", this->binary_values);
-        if (this->binary_values & 1)
-            printf("Hay vibración\n");
-        else
-            printf("No hay vibración\n");
-        if (this->binary_values & 2)
-            printf("No hay obtaculo\n");
-        else
-            printf("Hay obstaculo\n");
-        if ((this->binary_values & 4))
-            printf("No luz\n");
-        else
-            printf("Hay luz\n");
-        if (this->binary_values & 8)
-            printf("No hay fuego\n");
-        else
-            printf("Hay fuego\n");
-        if (this->has_persons > 350)
-            printf("Personas detectadas\n");
-        else
-            printf("Personas NO detectadas\n");
-        if (this->has_sound > 600)
-            printf("Sonido detectado \n");
-        else
-            printf("Sonido NO detectado \n");
-        if (this->has_gas > 350)
-            printf("Gas detectado\n");
-        else
-            printf("Gas NO detectado\n");
-        if (this->has_oil < 500)
-            printf("Aceite detectado\n");
-        else
-            printf("Aceite NO detectado\n");
-        if (this->has_rain < 500)
-            printf("Lluvia detectado\n");
-        else
-            printf("Lluvia NO detectado\n");
-        printf("Humedad: %u\n", this->humidity);
-        printf("Temperatura: %u\n", this->temperature);
-    }
-    else
-    {
-        printf("Paquete con checksum incorrecto\n\n");
-        this->writeToBBDD = false;
-    }
-}
+        int lenghtBufferIn = 500;
+        char bufferIn[500];
+        sleep(1);
+        char activate = 1;
+        char output1 = 0;
+        char output2 = 0;
+        char output3 = 0;
+        char output4 = 0;
+        char toSend[] = "1 1 1 0 0 0     \n\0";
+        if (action != -1)
+        {
+            if (action & 0x01)
+                output1 = 1;
+            if (action & 0x02)
+                output2 = 1;
+            if (action & 0x04)
+                output3 = 1;
+            if (action & 0x08)
+                output4 = 1;
+        }
+        toSend[0] = activate + '0';
+        toSend[2] = output1 + '0';
+        toSend[4] = output2 + '0';
+        toSend[6] = output3 + '0';
+        toSend[8] = output4 + '0';
+        char checksum = activate + output1 + output2 + output3 + output4;
+        toSend[10] = checksum + '0';
+        printf("Se manda: %s", toSend);
+        if ((write(this->arduinoConnection->fd, toSend, sizeof(toSend))) == -1)
+        {
+            fprintf(stderr, "write() failed: %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+        sleep(1);
+        this->arduinoConnection->read_from_serial(this->arduinoConnection->fd, bufferIn, 500);
+        if (this->deserialize(bufferIn, lenghtBufferIn))
+        {
+            printf("Action updated:  %s\n", bufferIn);
+            return 1;
+        }
+        printf("Fail to send action: %s\n", bufferIn);
+        return 0;
+    };
+};
 
 class BDconnection
 {
@@ -460,7 +486,10 @@ int main(void)
 {
     int numRows = 0;
     ArduinoConnection arduinoConnection((char *)"/dev/ttyACM0");
+    CallSensors callSensors(&arduinoConnection);
+    CallOutputs callOutputs(&arduinoConnection);
     BDconnection connection((char *)"dbname = raspberryTest");
+
     connection.getConnection(); // connect to bbdd
     int lastAction = 999;
     while (true)
@@ -468,16 +497,15 @@ int main(void)
         int auxAction = connection.getLastAction();
         if (lastAction != auxAction)
         {
-            if (arduinoConnection.sendActionToRaspberry(auxAction))
+            if (callOutputs.callToArduino(auxAction))
                 lastAction = auxAction;
         }
-        arduinoConnection.getSensorValues();
-        if (arduinoConnection.getWriteToBDD())
+        if (callSensors.callToArduino())
         {
-            connection.insertMeasurement(arduinoConnection.getBinaryValues(), arduinoConnection.getHasPersons(),
-                                         arduinoConnection.getHasSound(), arduinoConnection.getHasGas(), arduinoConnection.getHasOil(),
-                                         arduinoConnection.getHasRain(), arduinoConnection.getTemperature(), arduinoConnection.getHumidity());
-            arduinoConnection.resetWriteToBBDD();
+            connection.insertMeasurement(callSensors.getBinaryValues(), callSensors.getHasPersons(),
+                                         callSensors.getHasSound(), callSensors.getHasGas(), callSensors.getHasOil(),
+                                         callSensors.getHasRain(), callSensors.getTemperature(), callSensors.getHumidity());
+
             numRows++;
             printf("Número de filas introducidas: %d\n", numRows);
         }
