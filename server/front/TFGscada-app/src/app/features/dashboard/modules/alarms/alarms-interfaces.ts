b6dc52ddcc7eb569416208a72cfff307f182d93d
alberto <alberto.diez@bosonit.com>
2023-02-11 21:31:54 +0100

    feat: add interfaces

---
+++ server/front/TFGscada-app/src/app/features/dashboard/modules/alarms/alarms-interfaces.ts
@@ -0,0 +1,17 @@
+
+import { IUser } from '../../../dashboard/modules/users/users-interfaces';
+import { ISensor } from 'src/app/shared/interfaces/sersors.interfaces';
+import { IPlace } from '../places/places-interfaces';
+
+export interface IAlarm {
+    id: number;
+    date_time: Date;
+    sensorId: number;
+    operatorId: number;
+    date_finish: Date;
+    placeId: number;
+    sensor: ISensor | undefined;
+    operator: IUser | undefined;
+    place: IPlace | undefined;
+}
+
---
+++ server/front/TFGscada-app/src/app/shared/interfaces/sersors.interfaces.ts
@@ -0,0 +1,7 @@
+export interface ISensor {
+    id: number;
+    name: string;
+    description: string;
+    range_low: number;
+    range: number;
+}
