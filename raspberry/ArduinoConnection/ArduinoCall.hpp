df325adee5ffebaba67aaf76af0b984521fd2996
alberto <alberto.diez@bosonit.com>
2023-07-08 14:36:53 +0200

    refactor

--- raspberry/ArduinoConnection/ArduinoCall.hpp
+++ raspberry/ArduinoConnection/ArduinoCall.hpp
@@ -17,14 +17,6 @@ public:
         this->has_rain = this->temperature = this->humidity = 0;
     }
     int deserialize(char *buffer, int lenght);
-    int getBinaryValues() { return this->binary_values; };
-    int getHasPersons() { return this->has_persons; };
-    int getHasSound() { return this->has_sound; };
-    int getHasGas() { return this->has_gas; };
-    int getHasOil() { return this->has_oil; };
-    int getHasRain() { return this->has_rain; };
-    int getTemperature() { return this->temperature; };
-    int getHumidity() { return this->humidity; };
 };
 
 #endif
--- raspberry/ArduinoConnection/CallSensors.hpp
+++ raspberry/ArduinoConnection/CallSensors.hpp
@@ -27,6 +27,15 @@ public:
     {
     }
     bool callToArduino(int seconds);
+
+    int getBinaryValues() { return this->binary_values; };
+    int getHasPersons() { return this->has_persons; };
+    int getHasSound() { return this->has_sound; };
+    int getHasGas() { return this->has_gas; };
+    int getHasOil() { return this->has_oil; };
+    int getHasRain() { return this->has_rain; };
+    int getTemperature() { return this->temperature; };
+    int getHumidity() { return this->humidity; };
 };
 
 #endif
