242fc2afeefd584effe70eb80d170d6f963c5d2f
alberto <alberto.diez@bosonit.com>
2023-10-21 09:50:32 +0200

    refactor

---
+++ server/back/back/places_202310210949.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.places (latitude,longitude,address,identifier,pass,"actualizationTime") VALUES
+	 (42.46230448169748,-2.4514472709770523,'Avenida Perez Galdós','aaaaa','abc',5),
+	 (42.42795309363744,-2.41947541202865,'Plaza central, Villamediana de Iregua','Villamediana','aA@45678',20),
+	 (42.46054701798568,-2.4552900439949643,'Calle Chile','bbbbb','aA@45678',5);
---
+++ server/back/back/range_alarm_202310210949.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.range_alarm ("idSensor",name,description,range_low,range_high) VALUES
+	 (5,'Gas detectado','Se ha detectado un nivel alto de gas inflamable',300,1024),
+	 (8,'Temperatura excesiva','La temperatura es demasiado alta',50,200),
+	 (11,'Fuego detectado','Se ha detectado fuego en el sensor de fuego',1,1),
+	 (4,'Obstaculo detectado','Se ha detectado un obstaculo en el sensor',1,1);
